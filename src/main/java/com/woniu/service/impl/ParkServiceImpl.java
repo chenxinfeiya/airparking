acc73f3ee25c6b4a9ed1ec540f4118550f15be20
Giao <Giao@qq.com>
2019-07-18 15:05:14 +0800

    Yao:Add ParkServiceImpl and Impl Page

---
+++ src/main/java/com/woniu/service/impl/ParkServiceImpl.java
@@ -0,0 +1,35 @@
+package com.woniu.service.impl;
+
+import java.util.List;
+
+import com.woniu.model.Park;
+import com.woniu.service.IParkService;
+
+public class ParkServiceImpl implements IParkService {
+
+	@Override
+	public Park findOne(Integer parkid) {
+		return null;
+	}
+
+	@Override
+	public List<Park> findAll() {
+		return null;
+	}
+
+	@Override
+	public void add(Park park) {
+		
+	}
+
+	@Override
+	public void delete(Integer parkid) {
+		
+	}
+
+	@Override
+	public void update(Park park) {
+		
+	}
+
+}
