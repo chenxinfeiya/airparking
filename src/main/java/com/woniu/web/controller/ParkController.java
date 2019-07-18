38ccd0aa2d3388e67d7a4276974bb052a312d397
Giao <Giao@qq.com>
2019-07-18 16:38:33 +0800

    Yao: Complete the Foundation parkController

--- src/main/java/com/woniu/web/controller/ParkController.java
+++ src/main/java/com/woniu/web/controller/ParkController.java
@@ -1,5 +1,7 @@
 package com.woniu.web.controller;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.woniu.model.Message;
 import com.woniu.model.Park;
 import com.woniu.service.IParkService;
+import com.woniu.tools.CreateUUID;
 
 @RestController
 @RequestMapping()
@@ -19,6 +22,8 @@ public class ParkController {
 	private Message addPark(Park park) {
 		Message message = null;
 		try {
+			String uuid = CreateUUID.getUUID();
+			park.setParkid(uuid);
 			parkServiceImpl.add(park);
 			message = new Message(true, "车位添加成功！");
 		} catch (Exception e) {
@@ -28,5 +33,48 @@ public class ParkController {
 			return message;
 		}
 	}
+	
+	@RequestMapping()
+	private Message delPark(Park park) {
+		Message message = null;
+		try {
+			parkServiceImpl.delete(park);
+			message = new Message(true, "车位删除成功！");
+		} catch (Exception e) {
+			message = new Message(true, "车位删除失败！" + e);
+			throw new RuntimeException(e);
+		} finally {
+			return message;
+		}
+	}
+	
+	@RequestMapping()
+	private Message updatePark(Park park) {
+		Message message = null;
+		try {
+			parkServiceImpl.update(park);
+			message = new Message(true, "车位修改成功！");
+		} catch (Exception e) {
+			message = new Message(true, "车位修改失败！" + e);
+			throw new RuntimeException(e);
+		} finally {
+			return message;
+		}
+	}
+	
+	@RequestMapping
+	private Park findOne(String parkid) {
+		Park findOne = parkServiceImpl.findOne(parkid);
+		return findOne;
+	}
+	
+	@RequestMapping()
+	private List findALL() {
+		List<Park> parks = parkServiceImpl.findAll();
+		return parks;
+	}
+	
+	
+	
 	
 }
