989ccb72297e7cf094908dd2c449e37d0ffc881d
cx <cxfy@aaa.com>
2019-07-18 15:57:35 +0800

    chenxin:message

--- src/main/java/com/woniu/web/controller/CarController.java
+++ src/main/java/com/woniu/web/controller/CarController.java
@@ -1,5 +1,19 @@
 package com.woniu.web.controller;
 
+import javax.annotation.Resource;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.woniu.model.Message;
+import com.woniu.service.ICarService;
+
+@RestController
 public class CarController {
 
+	@Resource
+	private ICarService carServiceImpl;
+	
+//	private Message save() {
+//		
+//	}
 }
