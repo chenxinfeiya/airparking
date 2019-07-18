6d6684d64b66661731a0950961fef626dec0026d
cx <cxfy@aaa.com>
2019-07-18 18:23:17 +0800

    chenxin : redis

--- src/main/java/com/woniu/cache/MyRedisCache.java
+++ src/main/java/com/woniu/cache/MyRedisCache.java
@@ -1,15 +1,26 @@
 package com.woniu.cache;
 
 import org.apache.ibatis.cache.Cache;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import com.woniu.ApplicationContextHolder;
 
 public class MyRedisCache implements Cache{
-
+	private String id;
+	
+	private RedisTemplate<Object, Object> redisTemplate;
 	@Override
 	public String getId() {
-		// TODO Auto-generated method stub
-		return null;
+		return id;
 	}
-
+	
+	public MyRedisCache(final String id) {
+		if(id == null) {
+			throw new RuntimeException("null id is exception");
+		}
+		this.id = id;
+		redisTemplate = (RedisTemplate<Object, Object>) ApplicationContextHolder.getBean("redisTemplate");
+	}
 	@Override
 	public void putObject(Object key, Object value) {
 		// TODO Auto-generated method stub
