b27624bc061aa36c6a616b56e84f16195aea365c
gang <957001352@qq.com>
2019-07-18 15:41:26 +0800

    cg

--- src/main/java/com/woniu/service/IUserService.java
+++ src/main/java/com/woniu/service/IUserService.java
@@ -2,12 +2,12 @@ package com.woniu.service;
 
 import java.util.List;
 
-import com.woniu.model.Park;
+import com.woniu.model.User;
 
 public interface IUserService {
-	Park findOne(Integer parkid);
-	List<Park> findAll();
-	void add(Park park);
-	void delete(Integer parkid);
-	void update(Park park);
+	User findOne(Integer userid);
+	List<User> findAll();
+	void add(User user);
+	void delete(Integer userid);
+	void update(User user);
 }
