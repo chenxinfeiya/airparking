4a15c244a3b8a3efc5d42f01cd10d5f79c772ab1
cx <cxfy@aaa.com>
2019-07-18 17:47:28 +0800

    chenxin : redis

---
+++ src/main/java/com/woniu/ApplicationContextHolder.java
@@ -0,0 +1,22 @@
+package com.woniu;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class ApplicationContextHolder implements ApplicationContextAware{
+	private static ApplicationContext ctx;
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		ctx = applicationContext;
+	}
+	
+	public static <T> T getBean(Class<T> t) {
+		return ctx.getBean(t);
+	}
+	
+	public static Object getBean(String name) {
+		return ctx.getBean(name);
+	}
+}
---
+++ src/main/java/com/woniu/cache/MyRedisCache.java
@@ -0,0 +1,43 @@
+package com.woniu.cache;
+
+import org.apache.ibatis.cache.Cache;
+
+public class MyRedisCache implements Cache{
+
+	@Override
+	public String getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void putObject(Object key, Object value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object getObject(Object key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object removeObject(Object key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
