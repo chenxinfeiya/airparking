5d69b76a69ae5b1e0ef3964ff9f795681a932e10
wyx <wyx@163.com>
2019-07-19 14:41:30 +0800

    大王：重新提交

---
+++ src/main/java/com/woniu/web/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.woniu.web.controller;
+
+import java.util.UUID;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.woniu.model.User;
+import com.woniu.tools.CreateUUID;
+import com.woniu.tools.MD5;
+
+@Controller
+@RequestMapping
+public class UserController {
+	@RequestMapping("save")
+	public String save(User user) {
+		user.setUserid(CreateUUID.getUUID());
+		user.setUsername(user.getUsername());
+		user.setUserpass(MD5.md5s(user.getUserpass()));
+		user.setUserphone(user.getUserphone());
+		user.setUsersex(user.getUsersex());
+		user.setUseraddress(user.getUseraddress());
+		return null;
+		
+	}
+}
