67ed92cec6af1b430b8a7069de3abc41eb6bbbd2
cx <cxfy@aaa.com>
2019-07-22 16:47:34 +0800

    chenxin : shanchu

--- src/main/java/com/woniu/AirparkingApplication.java
+++ src/main/java/com/woniu/AirparkingApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AirparkingApplication {
 
 	public static void main(String[] args) {
+		 
 		SpringApplication.run(AirparkingApplication.class, args);
 	}
 
