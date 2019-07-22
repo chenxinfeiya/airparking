a445c752f7641f831d36650b1059a7404cab4077
cx <cxfy@aaa.com>
2019-07-22 16:56:53 +0800

    ..

--- src/main/java/com/woniu/AirparkingApplication.java
+++ src/main/java/com/woniu/AirparkingApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AirparkingApplication {
 
 	public static void main(String[] args) {
-		 
 		SpringApplication.run(AirparkingApplication.class, args);
 	}
 
