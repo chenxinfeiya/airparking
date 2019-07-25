5bb235791610f71896ea350b30ad0efcebf06103
wyx <wyx@163.com>
2019-07-25 14:51:26 +0800

    wxy:未完成

--- src/main/java/com/woniu/service/IPropertyService.java
+++ src/main/java/com/woniu/service/IPropertyService.java
@@ -2,13 +2,13 @@ package com.woniu.service;
 
 import java.util.List;
 
+import com.woniu.model.Park;
 import com.woniu.model.Porder;
-import com.woniu.model.Property;
 import com.woniu.model.User;
 
 
-public interface IProperty {
+public interface IPropertyService {
 	void update(User user);
 	User findOne(String userid);
-	List<Porder> findAll(String propertyid);
+	List<Park> findAll();
 }
